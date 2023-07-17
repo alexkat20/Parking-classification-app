643bff68f7a8e4f3341cca61be082ea3e2c79ace
alexkat20 <alexkat20@rambler.ru>
2023-07-17 09:16:47 +0300

    Refactored main file and renamed it

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.9
+ADD app.py /
+COPY . /app
+WORKDIR /app
+RUN pip3 install -r requirements.txt
+EXPOSE 5000
+CMD [ "python3", "./app.py" ]
---
+++ app.py
@@ -0,0 +1,59 @@
+from joblib import load
+from timm.data import resolve_data_config
+from timm.data.transforms_factory import create_transform
+import cv2
+
+#  import easyocr
+import numpy as np
+import torch
+from flask import Flask, request, jsonify
+from PIL import Image
+
+app = Flask(__name__)
+
+
+carplate_haar_cascade = cv2.CascadeClassifier("haarcascade_russian_plate_number.xml")
+
+
+@app.route("/predict1", methods=["POST"])
+def process_image_1():
+    for file in request.files:
+        print(file)
+
+    file = request.files["image"]
+    # Read the image via file.stream
+    #  img = Image.open(file.stream)
+
+    img = Image.open(file.stream).convert("RGB")
+    #  Neural network check
+    #  model1 = load('new_model1.joblib')
+    #  model2 = load('new_model2.joblib')
+    model3 = load("new_model8.joblib")
+
+    #  config1 = resolve_data_config({}, model=model1)
+    #  config2 = resolve_data_config({}, model=model2)
+    config3 = resolve_data_config({}, model=model3)
+
+    #  transform1 = create_transform(**config1)
+    #  transform2 = create_transform(**config2)
+    transform3 = create_transform(**config3)
+
+    #  tensor1 = transform1(img).unsqueeze(0)  # transform and add batch dimension
+    #  tensor2 = transform2(img).unsqueeze(0)  # transform and add batch dimension
+    tensor3 = transform3(img).unsqueeze(0)  # transform and add batch dimension
+
+    #  out1 = model1(tensor1.to('cpu'))
+    #  _, predicted1 = torch.max(out1, 1)
+
+    # out2 = model2(tensor2.to('cpu'))
+    # _, predicted2 = torch.max(out2, 1)
+
+    out3 = model3(tensor3.to("cpu"))
+    _, predicted3 = torch.max(out3, 1)
+    return jsonify({"prediction": int(predicted3[0])})
+
+    #  return jsonify({'msg': 'success', 'predictions': [int(predicted1[0]), int(predicted3[0]), int(predicted3[0])]})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
